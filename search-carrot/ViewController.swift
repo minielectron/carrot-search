097382bb9ecedad09ddd2e082025b61b45ef0c88
prakash sharma <prakash.sharma1@telekom-digital.com>
2022-08-05 10:33:30 +0530

    Add initial setup and data source method

--- search-carrot/ViewController.swift
+++ search-carrot/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    func getLabels() -> [[String]] {
+        let carrot = "🥕"
+        let rat = "🐱" 
+        let blank = "🌱"
+        
+        let carrotPosX = Int.random(in: 0..<7)
+        let carrotPosY = Int.random(in: 0..<7)
+        let catPosX = Int.random(in: 0..<7)
+        let catPosY = Int.random(in: 0..<7)
+        
+        var grid = [[String]]()
+        
+        for i in 0...7 {
+            for j in 0...7{
+                if i == carrotPosX && j == carrotPosY {
+                    grid[i][j] = carrot
+                    continue
+                }
+                
+                if i == catPosX && j==catPosY {
+                    grid[i][j] = rat
+                    continue
+                }
+                
+                grid[i][j] = blank
+            }
+        }
+        
+        return grid
+        
+    }
 }
 
